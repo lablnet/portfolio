f65d18bf0e352400fe50cd3ac9d86036ac66f971
lablnet <mumerfarooqlablnet01@gmail.com>
2020-11-03 18:21:30 +0500

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   chainWebpack: config => config.resolve.symlinks(false),
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/'
+    ? '/' : '/'
 }
