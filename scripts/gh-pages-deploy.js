6061d732b654d7313d74e07e1065d33557aa7533
lablnet <mumerfarooqlablnet01@gmail.com>
2020-11-03 18:19:05 +0500

    restructured

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -1,13 +1,10 @@
-/* eslint-disable no-console */
 const execa = require("execa");
 const fs = require("fs");
 (async () => {
   try {
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
-    // eslint-disable-next-line no-console
     console.log("Building started...");
     await execa("npm", ["run", "build"]);
-    // Understand if it's dist or build folder
     const folderName = fs.existsSync("dist") ? "dist" : "build";
     await execa("git", ["--work-tree", folderName, "add", "--all"]);
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
@@ -18,7 +15,6 @@ const fs = require("fs");
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
   } catch (e) {
-    // eslint-disable-next-line no-console
     console.log(e.message);
     process.exit(1);
   }
