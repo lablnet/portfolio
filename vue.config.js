a6886cf7a8353dba1942684a722a9d50fca6ca40
lablnet <mumerfarooqlablnet01@gmail.com>
2020-11-03 18:02:02 +0500

    deploy to gh page

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  chainWebpack: config => config.resolve.symlinks(false)
+  chainWebpack: config => config.resolve.symlinks(false),
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/' + process.env.CI_PROJECT_NAME + '/'
+    : '/'
 }
